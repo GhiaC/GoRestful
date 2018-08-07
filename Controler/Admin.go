a00d093539389765de35c9b82ace4d11a4ab903f
GhiaC <ghiasy@aut.ac.ir>
2018-08-07 17:05:02 +0430

    use create table

--- Controler/Admin.go
+++ Controler/Admin.go
@@ -12,7 +12,7 @@ func Admin(w http.ResponseWriter, r *http.Request) {
 func Status(w http.ResponseWriter, r *http.Request) {
 	if ok, _ := Authenticated(r); ok {
 		var users []Models.User
-		GetEngine().Table("users").Cols("id", "username").Find(&users)
+		GetEngine().Table("user").Cols("id", "username").Find(&users)
 		//if err == nil {
 		result := Models.StatusPageVariables{Users: users}
 		OpenTemplate(w, r, result, "status.html",Models.HeaderVariables{Title:"Users"})
